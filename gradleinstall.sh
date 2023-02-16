82d48ab4511860cc37d2d900646912ad6a0778f6
powerpow3r <109945886+powerpow3r@users.noreply.github.com>
2023-02-16 16:27:53 +0200

    Update gradleinstall.sh

--- gradleinstall.sh
+++ gradleinstall.sh
@@ -2,3 +2,4 @@ wget https://services.gradle.org/distributions/gradle-3.4.1-bin.zip
 sudo mkdir /opt/gradle
 sudo unzip -d /opt/gradle gradle-3.4.1-bin.zip
 echo "export PATH=$PATH:/opt/gradle/gradle-3.4.1/bin" >> ~/.bashrc
+#test
