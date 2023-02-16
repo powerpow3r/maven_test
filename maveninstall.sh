490e5c0213b1885d82e829b2310d376bb60e7228
powerpow3r <109945886+powerpow3r@users.noreply.github.com>
2023-02-16 16:07:31 +0200

    1

--- maveninstall.sh
+++ maveninstall.sh
@@ -5,3 +5,4 @@ set -e
 sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
 sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
 sudo yum install -y apache-maven
+# test
